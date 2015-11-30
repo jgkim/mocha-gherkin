d4db892d30ec157fda1168bf8d69f660f282709d
James G. Kim <jgkim@jayg.org>
2015-11-30 20:53:14 +0900

    Removed the babel plugin for `source-map-support`

---
+++ gulpfile.babel.js
@@ -0,0 +1,18 @@
+import gulp from 'gulp';
+import header from 'gulp-header';
+import sourcemaps from 'gulp-sourcemaps';
+import babel from 'gulp-babel';
+
+gulp.task('default', ['build']);
+
+gulp.task('build', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(header('import \'source-map-support/register\'\n'))
+    .pipe(babel({
+      'presets': ['es2015'],
+      'plugins': ['add-module-exports'],
+    }))
+    .pipe(sourcemaps.write('.', { 'sourceRoot': '../src' }))
+    .pipe(gulp.dest('build'));
+});
