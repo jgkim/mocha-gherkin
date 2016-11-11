59bdc8c0ae9f1cde076b62cfd3ba154cd4a025da
James G. Kim <jgkim@jayg.org>
2016-11-11 15:21:27 +0900

    Upgraded the dependencies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,16 +3,13 @@ import header from 'gulp-header';
 import sourcemaps from 'gulp-sourcemaps';
 import babel from 'gulp-babel';
 
-gulp.task('default', ['build']);
-
-gulp.task('build', () => {
-  return gulp.src('src/**/*.js')
+gulp.task('build', () =>
+  gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(header('import \'source-map-support/register\'\n'))
     .pipe(babel({
-      'presets': ['es2015'],
-      'plugins': ['add-module-exports'],
+      presets: ['es2015'],
+      plugins: ['add-module-exports'],
     }))
-    .pipe(sourcemaps.write('.', { 'sourceRoot': '../src' }))
-    .pipe(gulp.dest('build'));
-});
+    .pipe(sourcemaps.write('.', { sourceRoot: '../src' }))
+    .pipe(gulp.dest('build')));
